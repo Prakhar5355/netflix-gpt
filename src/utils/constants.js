407206a86b6f8290a7e89046fd02c1e5701f7f19
Prakhar5355 <workwithprakhar27@gmail.com>
2024-09-12 00:52:04 +0530

    Integrated GPT api and memoization

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,8 +8,22 @@ export const USER_AVATAR =
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTYxMTIyNTNlMjkwZTkyMmQ4ZTc2NTYzODcyN2UzOCIsIm5iZiI6MTcyNTgyMTE0NS4zMzMwOTgsInN1YiI6IjY2ZGRlZmEzN2VjNzNlOTA3ZTQ5OTczYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SN2Wo8TOfBbwBj4dzNgSsXCrE-uSodxYPQ_oLoI8aoU'
+      Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
+      // Authorization: 'Bearer ' + process.env.REACT_APP_TMDB_KEY
     }
   };
 
   export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
+
+  export const BG_URL =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "spanish", name: "Spanish" },
+];
+
+
+
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY
